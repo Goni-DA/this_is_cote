3f7ceb64f3d6301ae4859c3b95db5d984724c506
이건희 <goni@igeonhuiui-MacBookAir.local>
2021-08-27 15:02:54 +0900

    initial_commit

---
+++ greedy_01.py
@@ -0,0 +1,23 @@
+n, m, k = map(int, '4 2 3'.split())
+
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+
+result = 0
+
+while True :
+    for i in range(k):
+        if m == 0:
+            break
+        result += first
+        m -= 1
+    if m == 0:
+        break
+    result += second
+    m -= 1
+
+print(result)
+
